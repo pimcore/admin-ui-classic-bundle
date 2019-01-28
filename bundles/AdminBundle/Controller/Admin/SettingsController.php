pimcore/admin-ui-classic-bundle

commit fcc19267d7e12467eff25de051cb989fb69fafaf

Refactor how robots.txt files get persisted (#3870)
* Refactor how robots.txt files get persisted. the current solution is unsuitable for a lot of sites with a proper deployment process

bundles/AdminBundle/Controller/Admin/SettingsController.php
@@ -1506,24 +1506,17 @@ class SettingsController extends AdminController
     /**
      * @Route("/robots-txt", methods={"GET"})
      *
-     * @param Request $request
-     *
      * @return JsonResponse
      */
-    public function robotsTxtGetAction(Request $request)
+    public function robotsTxtGetAction()
     {
         $this->checkPermission('robots.txt');
 
-        $robotsPath = $this->getRobotsTxtPath($request);
-
-        $data = '';
-        if (is_file($robotsPath)) {
-            $data = file_get_contents($robotsPath);
-        }
+        $config = Config::getRobotsConfig();
 
         return $this->adminJson([
             'success' => true,
-            'data' => $data,
+            'data' => $config,
             'onFileSystem' => file_exists(PIMCORE_WEB_ROOT . '/robots.txt')
         ]);
     }
@@ -1539,30 +1532,19 @@ class SettingsController extends AdminController
     {
         $this->checkPermission('robots.txt');
 
-        $robotsPath = $this->getRobotsTxtPath($request);
-        File::put($robotsPath, $request->get('data'));
+        $values = $request->get('data');
+        if (!is_array($values)) {
+            $values = [];
+        }
+
+        File::putPhpFile(
+            Config::locateConfigFile('robots.php'),
+            to_php_data_file_format($values)
+        );
 
         return $this->adminJson([
             'success' => true
         ]);
-    }
-
-    /**
-     * @param Request $request
-     *
-     * @return string
-     */
-    protected function getRobotsTxtPath(Request $request)
-    {
-        if ($request->get('site')) {
-            $siteSuffix = '-' . $request->get('site');
-        } else {
-            $siteSuffix = '-default';
-        }
-
-        $robotsPath = PIMCORE_CONFIGURATION_DIRECTORY . '/robots' . $siteSuffix . '.txt';
-
-        return $robotsPath;
     }
 
     /**

bundles/AdminBundle/Resources/public/js/pimcore/settings/robotstxt.js
@@ -13,35 +13,72 @@
 
 pimcore.registerNS("pimcore.settings.robotstxt");
 pimcore.settings.robotstxt = Class.create({
+    onFileSystem: false,
+    data: {},
 
     initialize: function(id) {
-
-        this.site = "";
-        this.data = {data: ""};
-
         this.getTabPanel();
         this.load();
     },
 
     load: function () {
+        this.panel.setLoading(true);
+
         Ext.Ajax.request({
             url: "/admin/settings/robots-txt",
-            params: {
-                site: this.site
-            },
             success: function (response) {
 
                 try {
                     var data = Ext.decode(response.responseText);
-                    if(data.success && this.editArea instanceof Ext.form.TextArea) {
-                        this.data = data;
-                        this.editArea.setValue(this.data.data);
+                    if(data.success) {
+                        this.data = data.data;
+                        this.onFileSystem = data.onFileSystem;
+
+                        this.loadSites();
                     }
                 } catch (e) {
 
                 }
             }.bind(this)
         });
+    },
+
+    loadSites: function() {
+        this.formPanel = new Ext.form.Panel({
+            layout: 'fit'
+        });
+
+        var items = [];
+
+        pimcore.globalmanager.get("sites").load(function(records) {
+            Ext.each(records, function(record) {
+                items.push(this.getEditPanel(record))
+            }.bind(this));
+
+
+            var buttons = [];
+
+            if (this.onFileSystem) {
+                buttons.push(t("robots_txt_exists_on_filesystem"));
+            }
+
+            buttons.push({
+                text: t("save"),
+                iconCls: "pimcore_icon_apply",
+                disabled: this.onFileSystem,
+                handler: this.save.bind(this)
+            });
+
+            this.formPanel.add({
+                xtype: 'tabpanel',
+                layout: 'fit',
+                items: items,
+                buttons: buttons
+            });
+
+            this.panel.add(this.formPanel);
+            this.panel.setLoading(false);
+        }.bind(this));
     },
 
     activate: function () {
@@ -59,7 +96,7 @@ pimcore.settings.robotstxt = Class.create({
                 border: false,
                 layout: "fit",
                 closable:true,
-                items: [this.getEditPanel()]
+                items: []
             });
 
             var tabPanel = Ext.getCmp("pimcore_panel_tabs");
@@ -77,73 +114,40 @@ pimcore.settings.robotstxt = Class.create({
         return this.panel;
     },
 
-    getEditPanel: function () {
+    getEditPanel: function (siteRecord) {
+        var editArea = new Ext.form.TextArea({
+            xtype: "textarea",
+            name: 'data['+siteRecord.get('id')+']',
+            value: this.data.hasOwnProperty(siteRecord.get('id')) ? this.data[siteRecord.getId('id')] : '',
+            width: "100%",
+            height: "100%",
+            style: "font-family: 'Courier New', Courier, monospace;",
+            disabled: this.onFileSystem
+        });
 
-        if (!this.editPanel) {
+        var editPanel = new Ext.Panel({
+            title: siteRecord.get('domain'),
+            layout: 'fit',
+            iconCls: 'pimcore_icon_robots',
+            bodyStyle: "padding: 10px;",
+            items: [editArea]
+        });
 
-            if(this.data.onFileSystem) {
-                this.editArea = new Ext.Panel({
-                    bodyStyle: "padding:50px;",
-                    html: t("robots_txt_exists_on_filesystem")
-                });
-            } else {
-                this.editArea = new Ext.form.TextArea({
-                    xtype: "textarea",
-                    name: "data",
-                    value: this.data.data,
-                    width: "100%",
-                    height: "100%",
-                    style: "font-family: 'Courier New', Courier, monospace;"
-                });
-            }
+        editPanel.on("bodyresize", function (el, width, height) {
+            editArea.setWidth(width-20);
+            editArea.setHeight(height-20);
+        });
 
-            this.editPanel = new Ext.Panel({
-                bodyStyle: "padding: 10px;",
-                items: [this.editArea],
-                tbar: ["->", {
-                    xtype: 'tbtext',
-                    text: t("select_site")
-                }, {
-                    xtype: "combo",
-                    store: pimcore.globalmanager.get("sites"),
-                    valueField: "id",
-                    displayField: "domain",
-                    triggerAction: "all",
-                    editable: false,
-                    listeners: {
-                        "select": function (el) {
-                            this.site = el.getValue();
-                            this.load();
-                        }.bind(this)
-                    }
-                }],
-                buttons: [{
-                    text: t("save"),
-                    iconCls: "pimcore_icon_apply",
-                    handler: this.save.bind(this)
-                }]
-            });
-            this.editPanel.on("bodyresize", function (el, width, height) {
-                this.editArea.setWidth(width-20);
-                this.editArea.setHeight(height-20);
-            }.bind(this));
-        }
-
-        return this.editPanel;
+        return editPanel;
     },
 
 
     save : function () {
-
         Ext.Ajax.request({
             url: "/admin/settings/robots-txt",
             method: "PUT",
-            params: {
-                data: this.editArea.getValue(),
-                site: this.site
-            },
+            params: this.formPanel.form.getFieldValues(),
             success: function (response) {
-
                 try {
                     var data = Ext.decode(response.responseText);
                     if(data.success) {

commit 3a57e0d4259ba1b6880fe01a9dd6252783d86f7a

[Robots.txt] fixed regressions/bugs introduced by #3870

bundles/AdminBundle/Controller/Admin/SettingsController.php
@@ -1513,6 +1513,7 @@ class SettingsController extends AdminController
         $this->checkPermission('robots.txt');
 
         $config = Config::getRobotsConfig();
+        $config = $config->toArray();
 
         return $this->adminJson([
             'success' => true,