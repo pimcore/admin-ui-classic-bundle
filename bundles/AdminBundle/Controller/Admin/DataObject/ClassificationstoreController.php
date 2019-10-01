pimcore/admin-ui-classic-bundle

commit 33daf29e66efd296aa05d698406c698263ce46ac

[Object] use prepared statement in ClassificationstoreController::addCollectionsAction()

bundles/AdminBundle/Controller/Admin/DataObject/ClassificationstoreController.php
@@ -929,11 +929,10 @@ class ClassificationstoreController extends AdminController implements EventedCo
 
         if ($ids) {
             $db = \Pimcore\Db::get();
-            $query = 'select * from classificationstore_groups g, classificationstore_collectionrelations c where colId IN (' . implode(',', $ids)
-                . ') and g.id = c.groupId';
-
             $mappedData = [];
-            $groupsData = $db->fetchAll($query);
+            $groupsData = $db->fetchAll('select * from classificationstore_groups g, classificationstore_collectionrelations c where colId IN (:ids) and g.id = c.groupId', [
+                'ids' => $ids
+            ]);
 
             foreach ($groupsData as $groupData) {
                 $mappedData[$groupData['id']] = $groupData;

commit 3de17910d93bafc8bedc7d627b4f0ddf0f75b59a

[DataObject] Classificationstore cannot add collections (#5025)
When adding a classification collection no groups were found.
Caused by an array of IDs being injected as query parameter for SQL IN().
Fixed by converting the array to comma separated values.
Also force the IDs to integer values.
Regression introduced in commit 33daf29e66efd296aa05d698406c698263ce46ac

bundles/AdminBundle/Controller/Admin/DataObject/ClassificationstoreController.php
@@ -931,7 +931,7 @@ class ClassificationstoreController extends AdminController implements EventedCo
             $db = \Pimcore\Db::get();
             $mappedData = [];
             $groupsData = $db->fetchAll('select * from classificationstore_groups g, classificationstore_collectionrelations c where colId IN (:ids) and g.id = c.groupId', [
-                'ids' => $ids
+                'ids' => implode(',', array_filter($ids, 'intval')),
             ]);
 
             foreach ($groupsData as $groupData) {