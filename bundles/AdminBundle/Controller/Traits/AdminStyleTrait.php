pimcore/admin-ui-classic-bundle

commit 7467ea31c2e7864e514eb8135f6e1cea95e7078e

Moved AdminStyleTrait to Pimcore\Bundle\AdminBundle\Controller\Traits\ namespace

bundles/AdminBundle/Controller/Traits/AdminStyleTrait.php
@@ -1,0 +1,42 @@
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @category   Pimcore
+ * @package    Element
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace Pimcore\Bundle\AdminBundle\Controller\Traits;
+
+trait AdminStyleTrait
+{
+    /**
+     * @param ElementInterface $element
+     * @param null|int $context
+     * @param array $data
+     *
+     * @throws \Exception
+     */
+    protected function addAdminStyle(ElementInterface $element, $context = null, &$data = [])
+    {
+        $adminStyle = Service::getElementAdminStyle($element, $context);
+        $data['icon'] = $adminStyle->getElementIcon() !== false ? $adminStyle->getElementIcon() : null;
+        $data['iconCls'] = $adminStyle->getElementIconClass() !== false ? $adminStyle->getElementIconClass() : null;
+        if ($adminStyle->getElementCssClass() !== false) {
+            if (!isset($data['cls'])) {
+                $data['cls'] = '';
+            }
+            $data['cls'] .= $adminStyle->getElementCssClass() . ' ';
+        }
+        $data['qtipCfg'] = $adminStyle->getElementQtipConfig();
+    }
+}

bundles/AdminBundle/Controller/Admin/AssetController.php
@@ -14,6 +14,7 @@
 
 namespace Pimcore\Bundle\AdminBundle\Controller\Admin;
 
+use Pimcore\Bundle\AdminBundle\Controller\Traits\AdminStyleTrait;
 use Pimcore\Bundle\AdminBundle\Helper\GridHelperService;
 use Pimcore\Controller\Configuration\TemplatePhp;
 use Pimcore\Controller\EventedControllerInterface;
@@ -45,7 +46,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AssetController extends ElementControllerBase implements EventedControllerInterface
 {
-    use Element\AdminStyleTrait;
+    use AdminStyleTrait;
     use ElementEditLockHelperTrait;
 
     /**

bundles/AdminBundle/Controller/Admin/DataObject/DataObjectController.php
@@ -15,6 +15,7 @@
 namespace Pimcore\Bundle\AdminBundle\Controller\Admin\DataObject;
 
 use Pimcore\Bundle\AdminBundle\Controller\Admin\ElementControllerBase;
+use Pimcore\Bundle\AdminBundle\Controller\Traits\AdminStyleTrait;
 use Pimcore\Bundle\AdminBundle\Helper\GridHelperService;
 use Pimcore\Controller\Configuration\TemplatePhp;
 use Pimcore\Controller\EventedControllerInterface;
@@ -46,7 +47,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class DataObjectController extends ElementControllerBase implements EventedControllerInterface
 {
-    use Element\AdminStyleTrait;
+    use AdminStyleTrait;
     use ElementEditLockHelperTrait;
 
     /**

bundles/AdminBundle/Controller/Admin/Document/DocumentController.php
@@ -15,6 +15,7 @@
 namespace Pimcore\Bundle\AdminBundle\Controller\Admin\Document;
 
 use Pimcore\Bundle\AdminBundle\Controller\Admin\ElementControllerBase;
+use Pimcore\Bundle\AdminBundle\Controller\Traits\AdminStyleTrait;
 use Pimcore\Config;
 use Pimcore\Controller\EventedControllerInterface;
 use Pimcore\Db;
@@ -23,7 +24,6 @@ use Pimcore\Event\AdminEvents;
 use Pimcore\Image\HtmlToImage;
 use Pimcore\Logger;
 use Pimcore\Model\Document;
-use Pimcore\Model\Element\AdminStyleTrait;
 use Pimcore\Model\Site;
 use Pimcore\Model\Version;
 use Pimcore\Routing\Dynamic\DocumentRouteHandler;

bundles/AdminBundle/Controller/Admin/Document/DocumentControllerBase.php
@@ -15,6 +15,7 @@
 namespace Pimcore\Bundle\AdminBundle\Controller\Admin\Document;
 
 use Pimcore\Bundle\AdminBundle\Controller\AdminController;
+use Pimcore\Bundle\AdminBundle\Controller\Traits\AdminStyleTrait;
 use Pimcore\Controller\EventedControllerInterface;
 use Pimcore\Logger;
 use Pimcore\Model;
@@ -30,7 +31,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 abstract class DocumentControllerBase extends AdminController implements EventedControllerInterface
 {
-    use Model\Element\AdminStyleTrait;
+    use AdminStyleTrait;
 
     /**
      * @param Request $request

commit 232a43c0717687819ec0b3776d37577803fce468

Fixed namespace issues caused by 7467ea31c2e7864e514eb8135f6e1cea95e7078e

bundles/AdminBundle/Controller/Traits/AdminStyleTrait.php
@@ -17,6 +17,9 @@
 
 namespace Pimcore\Bundle\AdminBundle\Controller\Traits;
 
+use Pimcore\Model\Element\ElementInterface;
+use Pimcore\Model\Element\Service;
+
 trait AdminStyleTrait
 {
     /**