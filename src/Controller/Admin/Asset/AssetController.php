c500760d2178a56a63873f1ebfffbf1c296902ab
aryaantony92 <arya.antony@pimcore.com>
2023-06-20 01:37:25 +0200

    Refactor tree node config to services

--- src/Controller/Admin/Asset/AssetController.php
+++ src/Controller/Admin/Asset/AssetController.php
@@ -44,6 +44,7 @@ use Pimcore\Model\Element\ValidationException;
 use Pimcore\Model\Metadata;
 use Pimcore\Model\Schedule\Task;
 use Pimcore\Tool;
+use Pimcore\Bundle\AdminBundle\Service\AssetService;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
@@ -73,6 +74,10 @@ class AssetController extends ElementControllerBase implements KernelControllerE
     use UserNameTrait;
 
     protected Asset\Service $_assetService;
+
+    public function __construct(protected AssetService $assetService)
+    {
+    }
 
     /**
      * @Route("/tree-get-root", name="pimcore_admin_asset_treegetroot", methods={"GET"})
@@ -725,123 +730,7 @@ class AssetController extends ElementControllerBase implements KernelControllerE
 
     protected function getTreeNodeConfig(ElementInterface $element): array
     {
-        /** @var Asset $asset */
-        $asset = $element;
-
-        $permissions =  $asset->getUserPermissions($this->getAdminUser());
-
-        $tmpAsset = [
-            'id' => $asset->getId(),
-            'key' => $element->getKey(),
-            'text' => htmlspecialchars($asset->getFilename()),
-            'type' => $asset->getType(),
-            'path' => $asset->getRealFullPath(),
-            'basePath' => $asset->getRealPath(),
-            'locked' => $asset->isLocked(),
-            'lockOwner' => $asset->getLocked() ? true : false,
-            'elementType' => 'asset',
-            'permissions' => [
-                'remove' => $permissions['delete'],
-                'settings' => $permissions['settings'],
-                'rename' => $permissions['rename'],
-                'publish' => $permissions['publish'],
-                'view' => $permissions['view'],
-                'list' => $permissions['list'],
-            ],
-        ];
-
-        $hasChildren = $asset->getDao()->hasChildren($this->getAdminUser());
-
-        // set type specific settings
-        if ($asset instanceof Asset\Folder) {
-            $tmpAsset['leaf'] = false;
-            $tmpAsset['expanded'] = !$hasChildren;
-            $tmpAsset['loaded'] = !$hasChildren;
-            $tmpAsset['permissions']['create'] = $permissions['create'];
-            $tmpAsset['thumbnail'] = $this->getThumbnailUrl($asset, ['origin' => 'treeNode']);
-        } else {
-            $tmpAsset['leaf'] = true;
-            $tmpAsset['expandable'] = false;
-            $tmpAsset['expanded'] = false;
-        }
-
-        $this->addAdminStyle($asset, ElementAdminStyleEvent::CONTEXT_TREE, $tmpAsset);
-
-        if ($asset instanceof Asset\Image) {
-            try {
-                $tmpAsset['thumbnail'] = $this->getThumbnailUrl($asset, ['origin' => 'treeNode']);
-
-                // we need the dimensions for the wysiwyg editors, so that they can resize the image immediately
-                if ($asset->getCustomSetting('imageDimensionsCalculated')) {
-                    $tmpAsset['imageWidth'] = $asset->getCustomSetting('imageWidth');
-                    $tmpAsset['imageHeight'] = $asset->getCustomSetting('imageHeight');
-                }
-            } catch (\Exception $e) {
-                Logger::debug('Cannot get dimensions of image, seems to be broken.');
-            }
-        } elseif ($asset->getType() == 'video') {
-            try {
-                if (\Pimcore\Video::isAvailable()) {
-                    $tmpAsset['thumbnail'] = $this->getThumbnailUrl($asset, ['origin' => 'treeNode']);
-                }
-            } catch (\Exception $e) {
-                Logger::debug('Cannot get dimensions of video, seems to be broken.');
-            }
-        } elseif ($asset->getType() == 'document') {
-            try {
-                // add the PDF check here, otherwise the preview layer in admin is shown without content
-                if (\Pimcore\Document::isAvailable() && \Pimcore\Document::isFileTypeSupported($asset->getFilename())) {
-                    $tmpAsset['thumbnail'] = $this->getThumbnailUrl($asset, ['origin' => 'treeNode']);
-                }
-            } catch (\Exception $e) {
-                Logger::debug('Cannot get dimensions of video, seems to be broken.');
-            }
-        }
-
-        $tmpAsset['cls'] = '';
-        if ($asset->isLocked()) {
-            $tmpAsset['cls'] .= 'pimcore_treenode_locked ';
-        }
-        if ($asset->getLocked()) {
-            $tmpAsset['cls'] .= 'pimcore_treenode_lockOwner ';
-        }
-
-        return $tmpAsset;
-    }
-
-    protected function getThumbnailUrl(Asset $asset, array $params = []): ?string
-    {
-        $defaults = [
-            'id' => $asset->getId(),
-            'treepreview' => true,
-            '_dc' => $asset->getModificationDate(),
-        ];
-
-        $params = array_merge($defaults, $params);
-
-        if ($asset instanceof Asset\Image) {
-            return $this->generateUrl('pimcore_admin_asset_getimagethumbnail', $params);
-        }
-
-        if ($asset instanceof Asset\Folder) {
-            return $this->generateUrl('pimcore_admin_asset_getfolderthumbnail', $params);
-        }
-
-        if ($asset instanceof Asset\Video && \Pimcore\Video::isAvailable()) {
-            return $this->generateUrl('pimcore_admin_asset_getvideothumbnail', $params);
-        }
-
-        if ($asset instanceof Asset\Document && \Pimcore\Document::isAvailable() && $asset->getPageCount()) {
-            return $this->generateUrl('pimcore_admin_asset_getdocumentthumbnail', $params);
-        }
-
-        if ($asset instanceof Asset\Audio) {
-            return '/bundles/pimcoreadmin/img/flat-color-icons/speaker.svg';
-        }
-
-        if ($asset instanceof Asset) {
-            return '/bundles/pimcoreadmin/img/filetype-not-supported.svg';
-        }
+        return $this->assetService->getTreeNodeConfig($element, $this->getAdminUser());
     }
 
     /**
@@ -1863,7 +1752,7 @@ class AssetController extends ElementControllerBase implements KernelControllerE
                     'type' => $asset->getType(),
                     'filename' => $asset->getFilename(),
                     'filenameDisplay' => htmlspecialchars($filenameDisplay),
-                    'url' => $this->getThumbnailUrl($asset),
+                    'url' => $this->assetService->getThumbnailUrl($asset),
                     'idPath' => $data['idPath'] = Element\Service::getIdPath($asset),
                 ];
             }
