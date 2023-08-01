7408393d8b71329c90dba36f29ec8fa051a92845
Jan Walther <jan.walther@blackbit.de>
2023-08-01 12:02:54 +0200

    support text selection for readonly wysiwyg fields

--- public/js/pimcore/object/tags/wysiwyg.js
+++ public/js/pimcore/object/tags/wysiwyg.js
@@ -121,7 +121,6 @@ pimcore.object.tags.wysiwyg = Class.create(pimcore.object.tags.abstract, {
         this.component.on("afterrender", function() {
             Ext.get(this.editableDivId).dom.setAttribute("contenteditable", "false");
         }.bind(this));
-        this.component.disable();
         return this.component;
     },
 
