9abc4be731bc3c6672f3100cc723662b2111bd3c
Corepex <christian.feldkirchner@pimcore.com>
2023-08-09 09:02:46 +0200

    removed htmlDecode

--- public/js/pimcore/asset/tree.js
+++ public/js/pimcore/asset/tree.js
@@ -1184,7 +1184,7 @@
              elementType: "asset",
              elementSubType: record.data.type,
              id: record.data.id,
-             default: Ext.util.Format.htmlDecode(record.data.key)
+             default: record.data.key
          };
          pimcore.elementservice.editElementKey(options);
      },
