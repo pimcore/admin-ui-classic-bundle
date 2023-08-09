5b513bcd32b4a979b3bab4169faa12ea25e36508
Divesh Pahuja <divesh.pahuja@pimcore.com>
2023-08-09 17:36:33 +0200

    Remove unused

--- src/Service/ElementService.php
+++ src/Service/ElementService.php
@@ -273,9 +273,6 @@ class ElementService implements ElementServiceInterface
             $element->getType() == 'hardlink') {
             $tmpNode['leaf'] = false;
             $tmpNode['expanded'] = !$hasChildren;
-        } elseif (method_exists($element, 'getDocumentTreeNodeConfig')) {
-            $tmp = $element->getDocumentTreeNodeConfig();
-            $tmpNode = array_merge($tmpNode, $tmp);
         } elseif (method_exists($element, 'getTreeNodeConfig')) { //for BC reasons
             $tmp = $element->getTreeNodeConfig();
             $tmpNode = array_merge($tmpNode, $tmp);
