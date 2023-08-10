3e692ecacbe77efd89ec863883af245022c43f0c
robertSt7 <robert.steinkellner@pimcore.com>
2023-08-10 14:51:04 +0200

    Fix: build ace-editor with webpack

--- public/assets/admin.js
+++ public/assets/admin.js
@@ -1,7 +1,3 @@
-import * as ace from "ace-builds/src-noconflict/ace";
-import "ace-builds/src-min-noconflict/ext-modelist";
-import "ace-builds/src-min-noconflict/theme-chrome";
-
 import * as L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-draw";
