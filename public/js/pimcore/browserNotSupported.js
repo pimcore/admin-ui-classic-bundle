f97c7f5475c4504cbe1b3793b8ee2af9c600e860
robertSt7 <robert.steinkellner@pimcore.com>
2023-08-24 07:21:19 +0200

    add private

--- public/js/pimcore/browserNotSupported.js
+++ public/js/pimcore/browserNotSupported.js
@@ -1,5 +1,8 @@
 document.getElementById('proceed_with_unsupported_browser').addEventListener('click', showLogin);
 
+/**
+ * @private
+ */
 function showLogin() {
     document.getElementById('loginform').style.display = 'block';
     document.getElementById('browserinfo').style.display = 'none';
