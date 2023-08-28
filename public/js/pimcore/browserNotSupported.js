c3fd9d4568e326d782e236b91a6088d92a877373
dvesh3 <divesh.pahuja@pimcore.com>
2023-08-28 16:48:16 +0200

    review changes

--- public/js/pimcore/browserNotSupported.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('proceed_with_unsupported_browser').addEventListener('click', showLogin);
-
-/**
- * @private
- */
-function showLogin() {
-    document.getElementById('loginform').style.display = 'block';
-    document.getElementById('browserinfo').style.display = 'none';
-}
