52dd507fb42b3bf09732de6387e7d2d0f9574e94
dvesh3 <divesh.pahuja@pimcore.com>
2023-09-08 12:36:01 +0200

    [AdminStyle] Allow element text modification on tree

--- src/Controller/Traits/AdminStyleTrait.php
+++ src/Controller/Traits/AdminStyleTrait.php
@@ -52,5 +52,8 @@ trait AdminStyleTrait
             $data['cls'] .= $adminStyle->getElementCssClass() . ' ';
         }
         $data['qtipCfg'] = $adminStyle->getElementQtipConfig();
+        if (method_exists($adminStyle, 'getElementText')) {
+            $data['text'] = $adminStyle->getElementText();
+        }
     }
 }
