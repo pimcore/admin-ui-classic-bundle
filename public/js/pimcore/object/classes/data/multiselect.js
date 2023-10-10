da6ca15f7285f7a6beb81ed1bc802e734130a45a
aryaantony92 <arya.antony@pimcore.com>
2023-10-10 14:05:36 +0200

    [DataObject][MultiSelect] Fix options provider UI

--- public/js/pimcore/object/classes/data/multiselect.js
+++ public/js/pimcore/object/classes/data/multiselect.js
@@ -282,20 +282,6 @@ pimcore.object.classes.data.multiselect = Class.create(pimcore.object.classes.da
                 editable: false,
                 forceSelection: true
             },
-            {
-                xtype: "textfield",
-                fieldLabel: t("options_provider_class"),
-                width: 600,
-                name: "optionsProviderClass",
-                value: datax.optionsProviderClass
-            },
-            {
-                xtype: "textfield",
-                fieldLabel: t("options_provider_data"),
-                width: 600,
-                value: datax.optionsProviderData,
-                name: "optionsProviderData"
-            },
             valueGrid
         ]);
 
