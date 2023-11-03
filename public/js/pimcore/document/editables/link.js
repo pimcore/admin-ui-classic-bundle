04ae873ece25ec1d682c25175e3c4266a38dbb5a
Divesh Pahuja <divesh.pahuja@pimcore.com>
2023-11-03 16:11:52 +0100

    [Documents] Link - encode full path - Follow up to #41 (#330)

--- public/js/pimcore/document/editables/link.js
+++ public/js/pimcore/document/editables/link.js
@@ -92,7 +92,6 @@ pimcore.document.editables.link = Class.create(pimcore.document.editable, {
 
 
     getLinkContent: function () {
-
         let text = "[" + t("not_set") + "]";
         if (this.data.text) {
             text = this.data.text;
@@ -101,7 +100,7 @@ pimcore.document.editables.link = Class.create(pimcore.document.editable, {
         }
         let displayHtml = Ext.util.Format.htmlEncode(text);
         if (this.data.path || this.data.anchor || this.data.parameters) {
-            let fullpath = this.data.path + (this.data.parameters ? '?' + Ext.util.Format.htmlEncode(this.data.parameters) : '') + (this.data.anchor ? '#' + Ext.util.Format.htmlEncode(this.data.anchor) : '');
+            let fullpath = Ext.util.Format.htmlEncode(this.data.path + (this.data.parameters ? '?' + this.data.parameters : '') + (this.data.anchor ? '#' + this.data.anchor : ''));
             let displayHtml = Ext.util.Format.htmlEncode(text);
             
             if (this.config.textPrefix !== undefined) {
