20c890fc72b87cc99f61e5a02ef4eec798a97ee5
JiaJia Ji <kingjia90@gmail.com>
2023-11-08 14:50:01 +0100

    renamed with switch

--- src/Controller/Admin/Asset/AssetController.php
+++ src/Controller/Admin/Asset/AssetController.php
@@ -616,6 +616,7 @@ class AssetController extends ElementControllerBase implements KernelControllerE
         $stream = fopen($_FILES['Filedata']['tmp_name'], 'r+');
         $asset->setStream($stream);
         $asset->setCustomSetting('thumbnails', null);
+        $asset->getEmbeddedMetaData(true);
         $asset->setUserModification($this->getAdminUser()->getId());
 
         $newFileExt = pathinfo($newFilename, PATHINFO_EXTENSION);
