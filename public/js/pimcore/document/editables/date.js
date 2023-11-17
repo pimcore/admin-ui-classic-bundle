bbef8f366a78dd18b1e3f416c4d60730e9ee88a4
JiaJia Ji <kingjia90@gmail.com>
2023-11-17 12:43:32 +0100

    Update date.js

--- public/js/pimcore/document/editables/date.js
+++ public/js/pimcore/document/editables/date.js
@@ -29,7 +29,14 @@ pimcore.document.editables.date = Class.create(pimcore.document.editable, {
 
     render: function () {
         this.setupWrapper();
+        
+        if (this.config.format && this.config.format.includes('%')) {
+            console.warn('Deprecated: Date format contains % symbols which is used for strftime, please the use parameters format according Carbon isoFormat instead.');
 
+            // replace any % prefixed parts from strftime format
+            this.config.format = this.config.format.replace(/%([a-zA-Z])/g, '$1');
+        }
+        
         if(this.data) {
             this.config.value = this.data;
         }
