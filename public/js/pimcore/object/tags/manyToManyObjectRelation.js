031b69b36fe6403a9055c3ae70063642e837784d
Mindspace Devs <146089359+mindspace-devs@users.noreply.github.com>
2024-04-22 08:53:54 +0200

    [Bug]: Fix "getSaveData is not a function" in Folder context

--- public/js/pimcore/object/tags/manyToManyObjectRelation.js
+++ public/js/pimcore/object/tags/manyToManyObjectRelation.js
@@ -85,7 +85,7 @@ pimcore.object.tags.manyToManyObjectRelation = Class.create(pimcore.object.tags.
             storeConfig.autoLoad = true;
             storeConfig.listeners = {
                 beforeload: function(store) {
-                    store.getProxy().setExtraParam('unsavedChanges', this.object ? this.object.getSaveData().data : {});
+                    store.getProxy().setExtraParam('unsavedChanges', this.object && typeof this.object.getSaveData === "function" ? this.object.getSaveData().data : {});
                     store.getProxy().setExtraParam('context', JSON.stringify(this.getContext()));
                 }.bind(this)
             };
