834adb2282f8e9048bd5e0f5d806cb29da71a1b1
JiaJia Ji <kingjia90@gmail.com>
2024-08-26 17:09:49 +0200

    consider custom layout and saved grid values as base for field definition

--- src/Controller/Admin/DataObject/DataObjectHelperController.php
+++ src/Controller/Admin/DataObject/DataObjectHelperController.php
@@ -360,12 +360,9 @@ class DataObjectHelperController extends AdminAbstractController
                             'key' => $key,
                             'type' => 'system',
                             'label' => $key,
-                            'locked' => $sc['locked'] ?? null,
                             'position' => $sc['position'],
                         ];
-                        if (isset($sc['width'])) {
-                            $colConfig['width'] = $sc['width'];
-                        }
+                        $this->injectCustomLayoutValues($colConfig, $sc);
                         $availableFields[] = $colConfig;
                     } else {
                         $keyParts = explode('~', $key);
@@ -387,9 +384,7 @@ class DataObjectHelperController extends AdminAbstractController
                                         if ($fieldConfig) {
                                             $fieldConfig['key'] = $key;
                                             $fieldConfig['label'] = '#' . $keyFieldDef->getTitle();
-                                            if (isset($sc['locked'])) {
-                                                $fieldConfig['locked'] = $sc['locked'];
-                                            }
+                                            $fieldConfig = $this->injectCustomLayoutValues($fieldConfig, $sc);
                                             $availableFields[] = $fieldConfig;
                                         }
                                     }
@@ -427,12 +422,7 @@ class DataObjectHelperController extends AdminAbstractController
                             if ($fd !== null) {
                                 $fieldConfig = $this->getFieldGridConfig($fd, $gridType, (string)$sc['position'], true, $keyPrefix, $class, $objectId);
                                 if (!empty($fieldConfig)) {
-                                    if (isset($sc['width'])) {
-                                        $fieldConfig['width'] = $sc['width'];
-                                    }
-                                    if (isset($sc['locked'])) {
-                                        $fieldConfig['locked'] = $sc['locked'];
-                                    }
+                                    $fieldConfig = $this->injectCustomLayoutValues($fieldConfig, $sc);
                                     $availableFields[] = $fieldConfig;
                                 }
                             }
@@ -457,12 +447,7 @@ class DataObjectHelperController extends AdminAbstractController
                                 if (!empty($fd)) {
                                     $fieldConfig = $this->getFieldGridConfig($fd, $gridType, (string)$sc['position'], true, null, $class, $objectId);
                                     if (!empty($fieldConfig)) {
-                                        if (isset($sc['width'])) {
-                                            $fieldConfig['width'] = $sc['width'];
-                                        }
-                                        if (isset($sc['locked'])) {
-                                            $fieldConfig['locked'] = $sc['locked'];
-                                        }
+                                        $fieldConfig = $this->injectCustomLayoutValues($fieldConfig, $sc);
                                         $availableFields[] = $fieldConfig;
                                     }
                                 }
@@ -528,6 +513,26 @@ class DataObjectHelperController extends AdminAbstractController
             'searchFilter' => $gridConfig['searchFilter'] ?? '',
             'filter' => $gridConfig['filter'] ?? [],
         ];
+    }
+
+    private function injectCustomLayoutValues(array $fieldConfig, array $savedColumn): array
+    {
+        $keys = ['width', 'locked'];
+        foreach ($keys as $key){
+            if (isset($savedColumn[$key])) {
+                $fieldConfig[$key] = $savedColumn[$key];
+            }
+        }
+
+        $fieldConfigKeys = ['noteditable'];
+        foreach ($fieldConfigKeys as $fieldConfigKey){
+            if (isset($savedColumn['fieldConfig']['layout'][$fieldConfigKey])) {
+                $setter = 'set' . ucfirst($fieldConfigKey);
+                $fieldConfig['layout']->$setter($savedColumn['fieldConfig']['layout'][$fieldConfigKey]);
+            }
+        }
+
+        return $fieldConfig;
     }
 
     /**
