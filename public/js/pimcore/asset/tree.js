9dc2cb57127a175d27289814d3c429f2e0b637ba
brainshaker <parupp@gmx.de>
2024-10-20 20:01:13 +0200

    [Bug]: Fix root elements not being assignable to pimcore_relation
    This is a regressiom of this fix: https://github.com/pimcore/pimcore/commit/3352e2e51f43dbe66b1055b7074db17d90b8bed1
    See the following comment for details: https://github.com/pimcore/pimcore/issues/17649#issuecomment-2420418409

--- public/js/pimcore/asset/tree.js
+++ public/js/pimcore/asset/tree.js
@@ -80,7 +80,6 @@
 
          rootNodeConfig.text = rootNodeConfigText;
          rootNodeConfig.allowDrag = true;
-         rootNodeConfig.id = "" + rootNodeConfig.id;
          rootNodeConfig.iconCls = rootNodeConfigIconCls;
          rootNodeConfig.cls = "pimcore_tree_node_root";
          rootNodeConfig.expanded = true;
