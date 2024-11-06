a0eac6eec0df1ddde14b185d56c8d7c61529c304
Sebastian Blank <blank@data-factory.net>
2024-11-06 13:41:08 +0100

    Allow % widths

--- public/js/pimcore/element/helpers/gridColumnConfig.js
+++ public/js/pimcore/element/helpers/gridColumnConfig.js
@@ -449,8 +449,8 @@ pimcore.element.helpers.gridColumnConfig = {
 
             const title = t("filter_by_relation_field") + " " + fieldInfo.text;
             let width = 700;
-            if (tagType === 'manyToManyObjectRelation' && fieldInfo.layout.layout.width) {
-                width = fieldInfo.layout.layout.width + 25;
+            if (tagType === 'manyToManyObjectRelation' && fieldInfo.layout.layout.width && fieldInfo.layout.layout.width !== '100%') {
+                width = sumWidths(fieldInfo.layout.layout.width, 25);
             }
             this.filterByRelationWindow = new Ext.Window({
                 autoScroll: true,
