8a5f79c55ed45203795d40c3a3417b32c8ecc374
Sebastian Blank <blank@data-factory.net>
2024-12-09 10:22:17 +0100

    Fix: Video Element not working

--- public/js/pimcore/document/editables/video.js
+++ public/js/pimcore/document/editables/video.js
@@ -20,7 +20,7 @@ pimcore.document.editables.video = Class.create(pimcore.document.editable, {
     initialize: function($super, id, name, config, data, inherited) {
         $super(id, name, config, data, inherited);
 
-        data.allowedTypes = config.allowedTypes;
+        data.allowedTypes = config.allowedTypes ?? data.allowedTypes;
         this.data = data;
     },
 
