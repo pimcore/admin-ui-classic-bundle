b843151b4ee86981d9f0161fc766be7d858e8be4
mattamon <matthias.schuhmayer@pimcore.com>
2025-02-25 12:29:48 +0100

    Add getContainerExtension method

--- src/PimcoreAdminBundle.php
+++ src/PimcoreAdminBundle.php
@@ -21,15 +21,22 @@ use Pimcore\Bundle\AdminBundle\DependencyInjection\Compiler\GDPRDataProviderPass
 use Pimcore\Bundle\AdminBundle\DependencyInjection\Compiler\ImportExportLocatorsPass;
 use Pimcore\Bundle\AdminBundle\DependencyInjection\Compiler\SerializerPass;
 use Pimcore\Bundle\AdminBundle\DependencyInjection\Compiler\TranslatorPass;
+use Pimcore\Bundle\AdminBundle\DependencyInjection\PimcoreAdminExtension;
 use Pimcore\Bundle\AdminBundle\GDPR\DataProvider\DataProviderInterface;
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 use Pimcore\HttpKernel\Bundle\DependentBundleInterface;
 use Pimcore\HttpKernel\BundleCollection\BundleCollection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\WebpackEncoreBundle\WebpackEncoreBundle;
 
 class PimcoreAdminBundle extends AbstractPimcoreBundle implements DependentBundleInterface
 {
+    public function getContainerExtension(): ExtensionInterface
+    {
+        return new PimcoreAdminExtension();
+    }
+
     public function build(ContainerBuilder $container): void
     {
         // auto-tag GDPR data providers
