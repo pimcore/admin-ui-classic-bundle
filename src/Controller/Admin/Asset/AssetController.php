eeae879f1b9614861f6a7ec565aced2a21afb553
Christian Fasching <fashxp@users.noreply.github.com>
2025-03-17 09:15:13 +0100

    Numerous fixes for getById calls (#867)
    * Numerous fixes for getById calls
    
    * Numerous fixes for getById calls

--- src/Controller/Admin/Asset/AssetController.php
+++ src/Controller/Admin/Asset/AssetController.php
@@ -647,7 +647,7 @@ class AssetController extends ElementControllerBase implements KernelControllerE
 
         if ($parentAsset->isAllowed('create')) {
             if (!$equalAsset) {
-                $asset = Asset::create($request->get('parentId'), [
+                $asset = Asset::create((int)$request->get('parentId'), [
                     'filename' => $request->get('name'),
                     'type' => 'folder',
                     'userOwner' => $this->getAdminUser()->getId(),
@@ -1697,10 +1697,10 @@ class AssetController extends ElementControllerBase implements KernelControllerE
         $limit = 10;
 
         if ($allParams['limit']) {
-            $limit = $allParams['limit'];
+            $limit = (int)$allParams['limit'];
         }
         if ($allParams['start']) {
-            $start = $allParams['start'];
+            $start = (int)$allParams['start'];
         }
 
         $conditionFilters = [];
@@ -1858,7 +1858,7 @@ class AssetController extends ElementControllerBase implements KernelControllerE
 
             // this is because the key can get the prefix "_copy" if the target does already exists
             if ($sessionBag['parentId']) {
-                $targetParent = Asset::getById($sessionBag['parentId']);
+                $targetParent = Asset::getById((int)$sessionBag['parentId']);
             } else {
                 $targetParent = Asset::getById((int) $request->get('targetParentId'));
             }
--- src/Controller/Admin/DataObject/DataObjectController.php
+++ src/Controller/Admin/DataObject/DataObjectController.php
@@ -1872,7 +1872,7 @@ class DataObjectController extends ElementControllerBase implements KernelContro
 
             // this is because the key can get the prefix "_copy" if the target does already exists
             if ($sessionBag['parentId']) {
-                $targetParent = DataObject::getById($sessionBag['parentId']);
+                $targetParent = DataObject::getById((int) $sessionBag['parentId']);
             } else {
                 $targetParent = DataObject::getById((int) $request->get('targetParentId'));
             }
