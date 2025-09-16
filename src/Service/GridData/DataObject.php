656f5c3c93a98f22c18c368ed2e4408eeec5530d
JiaJia Ji <kingjia90@gmail.com>
2025-09-16 10:50:03 +0200

    typo

--- src/Service/GridData/DataObject.php
+++ src/Service/GridData/DataObject.php
@@ -52,10 +52,7 @@ class DataObject extends Element
             $data['classname'] = $object->getClassName();
             $data['idPath'] = Service::getIdPath($object);
             $data['inheritedFields'] = [];
-            $data['permissions'] = $object->getUserPermissions($user);// to prevent malforded grids in case of empty fieldcollections         
-if ($def instanceof ClassDefinition\Data\Fieldcollections){             
-    $data[$dataKey] ??= '';                                             
-}                                                                       
+            $data['permissions'] = $object->getUserPermissions($user);                                                           
             $data['locked'] = $object->isLocked();
 
             if (is_null($fields)) {
