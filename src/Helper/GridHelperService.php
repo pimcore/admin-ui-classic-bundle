5ffc9db6a7cb372c5c42346bdbf709ed337627e3
kingjia90 <6014195+kingjia90@users.noreply.github.com>
2025-10-08 12:22:13 +0000

    Apply php-cs-fixer changes

--- src/Helper/GridHelperService.php
+++ src/Helper/GridHelperService.php
@@ -124,7 +124,7 @@ class GridHelperService
                             'fieldname' => $fieldName,
                             'groupId' => $groupId,
                             'keyId' => $keyid,
-                            'language' => $language
+                            'language' => $language,
                         ];
 
                         // Some fields need a secondary value, e.g. the unit of a QuantityValue
@@ -462,7 +462,7 @@ class GridHelperService
                     }
                     $alreadyJoined[$mappedKey] = 1;
 
-                    if (isset($featureJoin['secondaryValue'])){
+                    if (isset($featureJoin['secondaryValue'])) {
                         $secondValue = ' and ' . $mappedKey . '.value2 = ' . $db->quote($featureJoin['secondaryValue']);
                     }
 
