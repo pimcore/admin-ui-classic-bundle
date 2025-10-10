6114d66868cf5ceb2a3158c5ce2bff80523a139a
JiaJia Ji <kingjia90@gmail.com>
2025-10-10 12:07:24 +0200

    [Improvement]: Possibility to include Pimcore core version in JSON export filenames  (#1030)
    * Update filename format for JSON export response
    
    * add v prefix to version number
    
    * apply to all

--- src/DependencyInjection/PimcoreAdminExtension.php
+++ src/DependencyInjection/PimcoreAdminExtension.php
@@ -89,6 +89,7 @@ final class PimcoreAdminExtension extends Extension implements PrependExtensionI
         $container->setParameter(self::PARAM_DOCUMENTS_NOTES_EVENTS_TYPES, $config['documents']['notes_events']['types']);
         $container->setParameter('pimcore_admin.csrf_protection.excluded_routes', $config['csrf_protection']['excluded_routes']);
         $container->setParameter('pimcore_admin.admin_languages', $config['admin_languages']);
+        $container->setParameter('pimcore_admin.export', $config['export']);
         $container->setParameter('pimcore_admin.custom_admin_path_identifier', $config['custom_admin_path_identifier']);
         $container->setParameter('pimcore_admin.custom_admin_route_name', $config['custom_admin_route_name']);
         $container->setParameter('pimcore_admin.user', $config['user']);
