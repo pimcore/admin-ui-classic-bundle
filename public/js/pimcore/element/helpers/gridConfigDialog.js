23a65cc5b4653cbe2e85bcbb397fbecd2ef4b3f4
Blackbit <jan.walther@blackbit.de>
2025-10-13 13:31:03 +0300

    [Grid Options] Fixes for saving grid configuration with "save filters" (#963)
    * saved grid column config with "save filters":
    - restore filters after loading
    - store "save filters" in database
    - add translation for "save filters"
    
    * make it not requiring a migration in the core, revert part that display the clear filter button
    
    * "critical" sonarcloud fix
    
    ---------
    
    Co-authored-by: Ji Jia Jia <kingjia90@gmail.com>

--- public/js/pimcore/element/helpers/gridConfigDialog.js
+++ public/js/pimcore/element/helpers/gridConfigDialog.js
@@ -285,7 +285,7 @@ pimcore.element.helpers.gridConfigDialog = Class.create({
 
         this.saveFilters = new Ext.form.field.Checkbox(
             {
-                fieldLabel: "Save filters",
+                fieldLabel: t("save_filters"),
                 inputValue: true,
                 name: "saveFilters",
                 value: this.settings.saveFilters
