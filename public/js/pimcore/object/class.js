2fd64eff906c9e534ee52e426cd827e818549272
Ji Jia Jia <kingjia90@gmail.com>
2025-10-29 11:08:08 +0100

    less line breaks

--- public/js/pimcore/object/class.js
+++ public/js/pimcore/object/class.js
@@ -18,34 +18,13 @@ pimcore.registerNS("pimcore.object.klass");
 pimcore.object.klass = Class.create({
 
     forbiddenNames: [
-        "abstract",
-        "class",
-        "data",
-        "folder",
-        "list",
-        "permissions",
-        "resource",
-        "concrete",
-        "interface",
-        "service",
-        "fieldcollection",
-        "localizedfield",
-        "objectbrick",
-        "default",
-        "abstractobject",
-        "classdefinition",
-        "classdefinitioninterface",
-        "classificationstore",
-        "definitionmodifier",
-        "importdataserviceinterface",
-        "lazyloadedfieldsinterface",
-        "listing",
-        "objectawarefieldinterface",
-        "ownerawarefieldinterface",
-        "pregetvaluehookinterface",
+        "abstract", "class", "data", "folder", "list", "permissions", "resource", "concrete", "interface",
+        "service", "fieldcollection", "localizedfield", "objectbrick", "default",
+        "abstractobject", "classdefinition", "classdefinitioninterface", "classificationstore",
+        "definitionmodifier", "importdataserviceinterface", "lazyloadedfieldsinterface", "listing",
+        "objectawarefieldinterface", "ownerawarefieldinterface", "pregetvaluehookinterface",
         "selectoptionsinterface"
     ],
-
     initialize: function () {
 
         this.getTabPanel();
