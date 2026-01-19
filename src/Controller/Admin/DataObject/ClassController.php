4bcc030d0c6ed4997776b08437d6837911843f3e
Martin Janser <martin@duss-janser.ch>
2026-01-19 11:53:57 +0100

    Sort object bricks by name (#992)
    * Sort object bricks by name
    
    * Apply suggestions from code review
    
    Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>
    
    ---------
    
    Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

--- src/Controller/Admin/DataObject/ClassController.php
+++ src/Controller/Admin/DataObject/ClassController.php
@@ -1256,7 +1256,9 @@ class ClassController extends AdminAbstractController implements KernelControlle
             }
         }
 
+        usort($groups, static fn ($a, $b) => $a['text'] <=> $b['text']);
         foreach ($groups as $group) {
+            usort($group['children'], static fn ($a, $b) => $a['title'] <=> $b['title']);
             $definitions[] = $group;
         }
 
