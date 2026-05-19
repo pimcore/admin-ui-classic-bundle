eee7dc196dff9e8bc7b7753167e4ab72b1a5e7c0
Ji Jia Jia <kingjia90@gmail.com>
2026-05-19 15:09:00 +0200

    Update unserialize to use allowed_classes option Refactor unserialize call to include allowed_classes option for security.

--- src/Helper/Dashboard.php
+++ src/Helper/Dashboard.php
@@ -58,7 +58,8 @@ final class Dashboard
 
         if (empty($this->dashboards)) {
             if (is_file($this->getConfigFile())) {
-                $dashboards = Serialize::unserialize(file_get_contents($this->getConfigFile()));
+                $dashboardFile = file_get_contents($this->getConfigFile());
+                $dashboards = unserialize($dashboardFile, ['allowed_classes' => false]);
                 if (!empty($dashboards)) {
                     $this->dashboards = $dashboards;
                 }
