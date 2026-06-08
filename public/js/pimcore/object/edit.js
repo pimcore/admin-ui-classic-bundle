a7bd1fb926b68dc280e8148a1549a4e770b2a845
JiaJia Ji <kingjia90@gmail.com>
2026-06-08 10:09:24 +0200

    Fix item index assignment in block context

--- public/js/pimcore/object/edit.js
+++ public/js/pimcore/object/edit.js
@@ -91,6 +91,7 @@ pimcore.object.edit = Class.create({
                         newValue = newValue.map((item, index) => {
                             if('oIndex' in item)
                                 item.oIndex = index;
+                            return item;
                         });
                     }
                     values[currentField.getName()] = newValue;
